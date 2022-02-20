8e292edcdadcc0a1de197983441350c160cf911e
Osama El Hariri <osama.hariri.dev@gmail.com>
2022-02-20 17:26:15 +0200

    Hide android system ui

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import AppEntry from './src/AppEntry';
 
 const App = () => {
+  StatusBar.setHidden(true);
   return (
     <AppEntry />
   );
--- android/app/src/main/java/com/jsclientgame/MainActivity.java
+++ android/app/src/main/java/com/jsclientgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.jsclientgame;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,7 +16,23 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideNavigationBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideNavigationBar();
+        }
+    }
+
+    private void hideNavigationBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+    }
 }
