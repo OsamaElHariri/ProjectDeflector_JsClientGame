71037892e726092a3fa5911bf66b6ccc5d2d6261
Osama El Hariri <osama.hariri.dev@gmail.com>
2022-04-03 12:18:01 +0300

    Fix win overlay button

--- src/game/WinnerOverlay.tsx
+++ src/game/WinnerOverlay.tsx
@@ -3,25 +3,27 @@ import React, { useEffect, useState } from 'react';
 
 import {
     Animated,
-    DevSettings,
     Pressable,
     StyleSheet,
     Text,
     View,
 } from 'react-native';
 import PlainOverlay from '../lobby/PlainOverlay';
+import UserService from '../lobby/userService';
 import { usePlayer } from '../main_providers/player_provider';
 import { useSyncedAnimation } from '../main_providers/synced_animation';
 import { AppNavigation } from '../types/uiTypes';
 import { shouldUpdate } from './diffWatcher';
 import { useGameState } from './game_state_provider';
+import Spinner from './Spinner';
 
 const WinnerOverlay = () => {
     const theme = useTheme();
-    const { player } = usePlayer();
+    const { player, updatePlayer } = usePlayer();
     const { stateSubject } = useGameState();
     const nav = useNavigation<AppNavigation>();
     const { bounceAnim } = useSyncedAnimation();
+    const [isLoading, setIsLoading] = useState(false);
 
     const [state, setState] = useState({
         winner: stateSubject.value.winner,
@@ -47,8 +49,19 @@ const WinnerOverlay = () => {
         return <View></View>
     }
 
-    const backToMenu = () => {
-        DevSettings.reload();
+    const backToMenu = async () => {
+        if (!player || !updatePlayer || isLoading) return;
+        setIsLoading(true);
+
+        await UserService.refreshStats().catch(() => { });
+        const user = await UserService.getCurrentUser().catch(() => undefined);
+        if (!user) {
+            player.gameStats.games += 1;
+            updatePlayer({ ...player });
+        } else {
+            updatePlayer(user);
+        }
+        nav.navigate('Lobby');
     }
 
     const text = winner === player?.id ? 'YOU WON' : 'YOU LOST...';
@@ -61,12 +74,14 @@ const WinnerOverlay = () => {
         <Pressable
             style={{ ...styles.button, backgroundColor: theme.colors.background, borderColor: theme.colors.text, borderWidth: 2 }}
             onPress={backToMenu}>
-            <Animated.Text style={{
-                ...styles.buttonText,
-                fontSize: 14,
-                color: theme.colors.text,
-                transform: [{ scale: dampenedBounceAnim }]
-            }}>Back to Menu</Animated.Text>
+            {isLoading
+                ? <View style={{ width: 28, height: 28, alignSelf: 'center' }}><Spinner /></View>
+                : <Animated.Text style={{
+                    ...styles.buttonText,
+                    fontSize: 14,
+                    color: theme.colors.text,
+                    transform: [{ scale: dampenedBounceAnim }]
+                }}>Back to Menu</Animated.Text>}
         </Pressable>
     </PlainOverlay>
 }
@@ -76,12 +91,14 @@ const styles = StyleSheet.create({
         marginTop: 40,
         borderWidth: 4,
         paddingVertical: 8,
-        paddingHorizontal: 20,
+        width: 140,
+        height: 48,
     },
     buttonText: {
         fontWeight: 'bold',
         textAlignVertical: 'center',
         textAlign: 'center',
+        marginVertical: 4,
     }
 });
 
