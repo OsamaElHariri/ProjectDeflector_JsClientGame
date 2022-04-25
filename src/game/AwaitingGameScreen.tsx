fb9a3a12f976179ef9578b07d2ff4b59afcf53e4
Osama El Hariri <osama.hariri.dev@gmail.com>
2022-04-25 23:19:39 +0300

    Prompt to share game for long wait times

--- src/game/AwaitingGameScreen.tsx
+++ src/game/AwaitingGameScreen.tsx
@@ -1,13 +1,17 @@
 import { useNavigation, useTheme } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
+    Animated,
+    Easing,
     Pressable,
+    Share,
     StyleSheet,
     Text,
     View,
 } from 'react-native';
 import PlainOverlay from '../lobby/PlainOverlay';
 import { useAudio } from '../main_providers/audio_provider';
+import { useSyncedAnimation } from '../main_providers/synced_animation';
 import { useWsClient } from '../main_providers/ws_provider';
 import { NetworkRequestStatus } from '../network/types';
 import { AppNavigation } from '../types/uiTypes';
@@ -17,11 +21,15 @@ import Spinner from './Spinner';
 const AwaitingGameScreen = () => {
     const theme = useTheme();
     const audio = useAudio();
+    const { bounceAnim, restartAnim } = useSyncedAnimation();
     const clientConnection = useWsClient();
     const nav = useNavigation<AppNavigation>();
     const [networkStatus, setNetworkStatus] = useState<NetworkRequestStatus>('NONE');
+    const isMounted = useRef(true);
+    const longWaitScaleAnim = useRef(new Animated.Value(0)).current;
 
     useEffect(() => {
+        restartAnim();
         if (!clientConnection?.client) return;
 
         const sub = clientConnection.client.events().subscribe(async (evt) => {
@@ -30,13 +38,27 @@ const AwaitingGameScreen = () => {
             }
         });
 
-        const interval = setInterval(() => {
+        const touchQueueInterval = setInterval(() => {
+            if (!isMounted.current) return;
             GameService.touchQueue().catch(() => undefined);
-        }, 10000)
+        }, 10000);
+
+        setTimeout(() => {
+            if (!isMounted.current) return;
+            Animated.timing(longWaitScaleAnim,
+                {
+                    toValue: 1,
+                    duration: 400,
+                    easing: Easing.elastic(2),
+                    useNativeDriver: true,
+                }
+            ).start();
+        }, 7000);
 
         return () => {
+            isMounted.current = false;
             sub.unsubscribe();
-            clearInterval(interval);
+            clearInterval(touchQueueInterval);
         };
     }, []);
 
@@ -50,21 +72,59 @@ const AwaitingGameScreen = () => {
             });
     }
 
+    const shareGame = () => {
+        Share.share({
+            message: "Let's play some Hit Bounce\nhttps://play.google.com/store/apps/details?id=com.hitbounce.game",
+        }).catch(() => undefined);
+    }
+
+    const dampenedBounceAnim = Animated.add(0.5, Animated.multiply(0.5, bounceAnim));
+    const InviteFriend = () => <View style={{ alignItems: 'center' }}>
+        <Text style={{ color: theme.colors.text, textAlign: 'center', fontSize: 20, width: '75%' }}>
+            Wow... It looks like everyone else is still asleep. That's alright, you can invite your own friends to play!
+        </Text>
+        <View style={{ paddingTop: 20 }} />
+        <Pressable
+            style={{ ...styles.button, backgroundColor: theme.colors.background, borderColor: theme.colors.text, borderWidth: 2 }}
+            onPress={shareGame}>
+            <Animated.Text style={{
+                ...styles.buttonText,
+                fontWeight: 'bold',
+                color: theme.colors.text,
+                transform: [{ scale: dampenedBounceAnim }],
+            }}>
+                Invite Friend
+            </Animated.Text>
+        </Pressable>
+    </View>
+
+    const veryDampenedBounceAnim = Animated.add(0.7, Animated.multiply(0.3, bounceAnim));
     return (
         <PlainOverlay>
-            <Text style={{ fontWeight: 'bold', color: theme.colors.text, fontSize: 32 }}>Finding Match</Text>
-            <View style={{ paddingTop: 40 }} />
+            <View style={{ alignItems: 'center', flex: 1 }}>
+                <Text style={{ color: theme.colors.text, fontSize: 28 }}>Finding Match</Text>
+                <View style={{ paddingTop: 20 }} />
 
-            {networkStatus === 'LOADING'
-                ? <View style={{ width: 24, height: 24 }}><Spinner /></View>
-                : <Pressable
-                    style={{ ...styles.button, backgroundColor: theme.colors.background, borderColor: theme.colors.text, borderWidth: 2 }}
-                    onPress={cancelFindGame}>
-                    <Text style={{
-                        ...styles.buttonText,
-                        color: theme.colors.text
-                    }}>Cancel</Text>
-                </Pressable>}
+                {networkStatus === 'LOADING'
+                    ? <View style={{ width: 24, height: 24 }}><Spinner /></View>
+                    : <Pressable
+                        style={{ ...styles.button, backgroundColor: theme.colors.background, borderColor: theme.colors.text, borderWidth: 2 }}
+                        onPress={cancelFindGame}>
+                        <Animated.Text style={{
+                            ...styles.buttonText,
+                            color: theme.colors.text,
+                            transform: [{ scale: veryDampenedBounceAnim }],
+                        }}>Cancel</Animated.Text>
+                    </Pressable>}
+            </View>
+
+            <Animated.View style={{
+                flex: 1,
+                transform: [{ scale: longWaitScaleAnim }],
+            }}>
+                <InviteFriend />
+            </Animated.View>
+
         </PlainOverlay >
     );
 };
@@ -77,7 +137,6 @@ const styles = StyleSheet.create({
         borderRadius: 10,
     },
     buttonText: {
-        fontWeight: 'bold',
         fontSize: 16,
     }
 });
